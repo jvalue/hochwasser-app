ee98c571bbe0e3d1281efee6f0e98738e17ce7ef
Philipp Eichhorn <philipp.pe.eichhorn@studium.uni-erlangen.de>
2014-05-02 20:00:46 +0200

    Added simple stub buttons

--- src/de/bitdroid/flooding/MainActivity.java
+++ src/de/bitdroid/flooding/MainActivity.java
@@ -2,14 +2,32 @@ package de.bitdroid.flooding;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		Button updateBtn = (Button) findViewById(R.id.content_update);
+		updateBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Update stub!", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		Button clearBtn = (Button) findViewById(R.id.content_clear);
+		clearBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Clear stub!", Toast.LENGTH_SHORT).show();
+			}
+		});
     }
+
 }

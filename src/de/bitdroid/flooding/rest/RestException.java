7ba9c62a2825412c91514e6b1fd03f24d7bb1b2a
Philipp Eichhorn <philipp.pe.eichhorn@studium.uni-erlangen.de>
2014-05-06 17:46:53 +0200

    rest: added simple restcall and restexception

---
+++ src/de/bitdroid/flooding/rest/RestException.java
@@ -0,0 +1,46 @@
+package de.bitdroid.flooding.rest;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+
+public final class RestException extends Exception {
+	public static final long serialVersionUID = 42L;
+
+	public static final int UNSET  = -1;
+	private final int code;
+
+	public RestException(int code) {
+		this.code = code;
+	}
+
+	public RestException(IOException nestedException) {
+		super(nestedException);
+		this.code = RestException.UNSET;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	@Override
+	public String getMessage() {
+		Throwable cause = getCause();
+		if (cause != null) return "Failed to communicate with TradeCard servers (" + cause.getMessage() + ")";
+		
+		if (code == HttpURLConnection.HTTP_NOT_MODIFIED) {
+			return "The requested documents on TradeCard were not modified";
+		} else if (code == HttpURLConnection.HTTP_BAD_REQUEST) {
+			return "The TradeCard servers did not understand the request";
+		} else if (code == HttpURLConnection.HTTP_UNAUTHORIZED) {
+			return "The credentials provided were not correct";
+		} else if (code == HttpURLConnection.HTTP_FORBIDDEN) {
+			return "The request is not allowed to make these changes";
+		} else if (code == HttpURLConnection.HTTP_NOT_FOUND) {
+			return "The requested resource is not available in TradeCard";
+		} else if (code >= 500 && code <= 600) {
+			return "Unable to connec to the TradeCard servers";
+		}
+		return "Unknown error (" + code + ")";
+	}
+}

a1df791e10133f0a7435ef428d1e7c3f33bd0809
Philipp Eichhorn <philipp.pe.eichhorn@studium.uni-erlangen.de>
2014-05-08 14:47:23 +0200

    removed button from example view

--- src/de/bitdroid/flooding/MainActivity.java
+++ src/de/bitdroid/flooding/MainActivity.java
@@ -2,8 +2,6 @@ package de.bitdroid.flooding;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import de.bitdroid.flooding.ods.SyncUtils;
@@ -21,14 +19,6 @@ public class MainActivity extends Activity {
 		listAdapter = new StationsListAdapter(this);
 		ListView listView = (ListView) findViewById(R.id.content_list);
 		listView.setAdapter(listAdapter);
-
-		Button updateButton = (Button) findViewById(R.id.update_button);
-		updateButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				listAdapter.notifyDataSetChanged();
-			}
-		});
 
 		SyncUtils.setupSyncAdapter(this);
 

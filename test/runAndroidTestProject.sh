802fe70dddf20da166b07edef53789966e3f011d
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-07 19:59:32 +0200

    test: cleanup in runAndroidTestProject

--- test/runAndroidTestProject.sh
+++ test/runAndroidTestProject.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-cd ..
-adb shell pm uninstall -k de.bitdroid.flooding
-ant clean debug
-adb install -r bin/flooding-debug.apk
-cd - 
-ant clean debug
-adb install -r bin/flooding_test-debug.apk
+ant clean debug install
 ant test

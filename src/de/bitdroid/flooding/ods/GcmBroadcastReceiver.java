e4aa51b28318306fef854747104004704fa133fc
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-11 18:30:50 +0200

    ods: handle gcm debug msg in GcmBroadcastReceiver

--- src/de/bitdroid/flooding/ods/GcmBroadcastReceiver.java
+++ src/de/bitdroid/flooding/ods/GcmBroadcastReceiver.java
@@ -3,12 +3,20 @@ package de.bitdroid.flooding.ods;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.PowerManager;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 
+import de.bitdroid.flooding.utils.Log;
+
 
 public final class GcmBroadcastReceiver extends BroadcastReceiver {
+
+	private final static String 
+		DATA_KEY_SOURCE = "source",
+		DATA_KEY_DEBUG = "debug";
+
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
@@ -22,11 +30,29 @@ public final class GcmBroadcastReceiver extends BroadcastReceiver {
 			String messageType = GoogleCloudMessaging.getInstance(context).getMessageType(intent);
 
 			if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-				// work
+				String debug = intent.getStringExtra(DATA_KEY_DEBUG);
+				if (debug != null && !debug.equals("") && Boolean.valueOf(debug)) {
+					handleDebug(context, intent);
+					return;
+				}
 			}
 
 		} finally {
 			wl.release();
 		}
+	}
+
+
+	private void handleDebug(Context context, Intent intent) {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Push Notification received: \n\n");
+
+		Bundle extras = intent.getExtras();
+		for (String key : extras.keySet()) {
+			Object value = extras.get(key);
+			builder.append(key + ":\t" + value.toString() + "\n");
+		}
+
+		Log.debug(builder.toString());
 	}
 }

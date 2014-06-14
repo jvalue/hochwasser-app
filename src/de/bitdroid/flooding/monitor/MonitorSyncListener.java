76e265ef43101a39d5bfdbb1b27d61330a5007dc
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-14 22:29:50 +0200

    monitor: only copy db on sync success

--- src/de/bitdroid/flooding/monitor/MonitorSyncListener.java
+++ src/de/bitdroid/flooding/monitor/MonitorSyncListener.java
@@ -3,30 +3,26 @@ package de.bitdroid.flooding.monitor;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 import de.bitdroid.flooding.ods.OdsSource;
 import de.bitdroid.flooding.ods.SyncAdapter;
+import de.bitdroid.flooding.utils.Log;
 
 
 public final class MonitorSyncListener extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		String sourceName = intent.getExtras().getString(SyncAdapter.EXTRA_SOURCE_NAME);
+		String sourceName = intent.getStringExtra(SyncAdapter.EXTRA_SOURCE_NAME);
 		OdsSource source = OdsSource.fromString(sourceName);
-		if (SourceMonitor.getInstance(context).isBeingMonitored(source)) {
-			Toast.makeText(
-					context, 
-					"Sync finished!", 
-					Toast.LENGTH_LONG)
-				.show();
+		boolean success = intent.getBooleanExtra(SyncAdapter.EXTRA_SYNC_SUCCESSFUL, false);
 
+		if (SourceMonitor.getInstance(context).isBeingMonitored(source) && success) {
+			Log.info("Monitor is copying db");
 			Intent serviceIntent = new Intent(context, CopySourceService.class);
 			serviceIntent.putExtra(CopySourceService.EXTRA_SOURCE_NAME, source.toString());
 			context.startService(serviceIntent);
 		}
 	}
-
 
 }

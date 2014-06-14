f7397518c706d7371bfa772e0781ebea98777e59
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-14 21:50:36 +0200

    ods: added SyncStatusListener for monitoring sync status.

--- src/de/bitdroid/flooding/ods/SyncAdapter.java
+++ src/de/bitdroid/flooding/ods/SyncAdapter.java
@@ -15,7 +15,8 @@ public final class SyncAdapter extends AbstractThreadedSyncAdapter {
 	public static final String 
 			ACTION_SYNC_FINISH = "de.bitdroid.flooding.ods.ACTION_SYNC_FINISH",
 			ACTION_SYNC_ALL_FINISH = "de.bitdroid.flooding.ods.ACTION_SYNC_ALL_FINISH",
-			EXTRA_SOURCE_NAME = "EXTRA_SOURCE_NAME";
+			EXTRA_SOURCE_NAME = "EXTRA_SOURCE_NAME",
+			EXTRA_SYNC_SUCCESSFUL = "EXTRA_SYNC_SUCCESSFUL";
 
 	private final Context context;
 
@@ -43,57 +44,66 @@ public final class SyncAdapter extends AbstractThreadedSyncAdapter {
 
 		Log.debug("Sync started");
 
-		try {
+		boolean success = true;
 
-			String sourceName = extras.getString(EXTRA_SOURCE_NAME);
-			if (sourceName == null) {
-				// sync all resources and sources
-				for (OdsSource source : OdsSourceManager.getInstance(context).getPollingSources()) {
-					syncSource(provider, source);
-					sendSyncFinishBroadcast(source);
-				}
-
-			} else {
-				// sync single source
-				OdsSource source = OdsSource.fromString(sourceName);
-				syncSource(provider, source);
-				sendSyncFinishBroadcast(source);
+		String sourceName = extras.getString(EXTRA_SOURCE_NAME);
+		if (sourceName == null) {
+			// sync all resources and sources
+			for (OdsSource source : OdsSourceManager.getInstance(context).getPollingSources()) {
+				success  = success && syncSource(provider, source, syncResult);
 			}
 
-			sendSyncFinishBroadcast(null);
-			Log.debug("Sync finished");
+		} else {
+			// sync single source
+			OdsSource source = OdsSource.fromString(sourceName);
+			success = success && syncSource(provider, source, syncResult);
+		}
+
+		sendSyncFinishBroadcast(null, success);
+		Log.debug("Sync finished");
+	}
+
+
+	private boolean syncSource(
+			ContentProviderClient provider, 
+			OdsSource source,
+			SyncResult syncResult) {
+
+		boolean success = false;
+
+		try {
+			Log.debug("Syncing " +  source.getSourceUrlPath());
+			Processor processor = new Processor(provider, source);
+			String retString = new RestCall.Builder(
+					RestCall.RequestType.GET,
+					OdsSourceManager.getInstance(context).getOdsServerName())
+				.path(source.getSourceUrlPath())
+				.build()
+				.execute();
+
+			processor.processGetAll(retString);
+			success = true;
 
 		} catch (Exception e) {
 			syncResult.hasHardError();
 			Log.error(android.util.Log.getStackTraceString(e));
 		}
+
+		sendSyncFinishBroadcast(source, success);
+		return success;
 	}
 
 
-	private void syncSource(
-			ContentProviderClient provider, 
-			OdsSource source) throws Exception {
-
-		Log.debug("Syncing " +  source.getSourceUrlPath());
-		Processor processor = new Processor(provider, source);
-		String retString = new RestCall.Builder(
-				RestCall.RequestType.GET,
-				OdsSourceManager.getInstance(context).getOdsServerName())
-			.path(source.getSourceUrlPath())
-			.build()
-			.execute();
-
-		processor.processGetAll(retString);
-	}
-
-
-	private void sendSyncFinishBroadcast(OdsSource source) {
+	private void sendSyncFinishBroadcast(OdsSource source, boolean success) {
 		if (source != null) {
 			Intent syncFinishIntent = new Intent(ACTION_SYNC_FINISH);
 			syncFinishIntent.putExtra(EXTRA_SOURCE_NAME, source.getClass().getName());
+			syncFinishIntent.putExtra(EXTRA_SYNC_SUCCESSFUL,  success);
 			context.sendBroadcast(syncFinishIntent);
 		} else {
-			context.sendBroadcast(new Intent(ACTION_SYNC_ALL_FINISH));
+			Intent syncFinishIntent = new Intent(ACTION_SYNC_ALL_FINISH);
+			syncFinishIntent.putExtra(EXTRA_SYNC_SUCCESSFUL,  success);
+			context.sendBroadcast(syncFinishIntent);
 		}
 	}
 }
---
+++ src/de/bitdroid/flooding/ods/SyncStatusListener.java
@@ -0,0 +1,97 @@
+package de.bitdroid.flooding.ods;
+
+import java.util.Calendar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+
+public final class SyncStatusListener extends BroadcastReceiver {
+
+	private static final Object LOCK = new Object();
+
+	private static final String PREFS_NAME = "de.bitdroid.flooding.ods.SyncStatusListener";
+	private static final long PREFS_DEFAULT_TIMESTAMP = -1;
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String sourceString = intent.getStringExtra(SyncAdapter.EXTRA_SOURCE_NAME);
+		OdsSource source = OdsSource.fromString(sourceString);
+		boolean success = intent.getBooleanExtra(SyncAdapter.EXTRA_SYNC_SUCCESSFUL, false);
+
+		synchronized(LOCK) {
+			SharedPreferences.Editor editor = getSharedPreferences(context).edit();
+			editor.putLong(toKey(source, success), System.currentTimeMillis());
+			editor.commit();
+		}
+	}
+
+
+	/**
+	 * Returns a timestamp representing the last successful sync of this source,
+	 * or null if none was recorded.
+	 */
+	static Calendar getLastSucccessfulSync(Context context, OdsSource source) {
+		return getTimestamp(context, source, true);
+	}
+
+
+	/**
+	 * Returns a timestamp representing the last sync of this source,
+	 * or null if it has never been synced.
+	 */
+	static Calendar getLastFailedSync(Context context, OdsSource source) {
+		return getTimestamp(context, source, false);
+	}
+
+
+	/**
+	 * Convenience method for getting the last sync for the supplied source.
+	 * @return null if no sync ever occured.
+	 */
+	static Calendar getLastSync(Context context, OdsSource source) {
+		Calendar lastSuccess, lastFailure;
+		synchronized(LOCK) {
+			lastSuccess = getLastSucccessfulSync(context, source);
+			lastFailure = getLastFailedSync(context, source);
+		}
+
+		if (lastSuccess == null && lastFailure == null) return null;
+		if (lastSuccess == null) return lastFailure;
+		if (lastFailure == null) return lastSuccess;
+		if (lastSuccess.after(lastFailure)) return lastSuccess;
+		return lastFailure;
+	}
+
+
+
+	private static Calendar getTimestamp(Context context, OdsSource source, boolean success) {
+		synchronized(LOCK) {
+			SharedPreferences prefs = getSharedPreferences(context);
+			long timestamp = prefs.getLong(toKey(source, success), PREFS_DEFAULT_TIMESTAMP);
+			if (timestamp == PREFS_DEFAULT_TIMESTAMP) return null;
+
+			Calendar c = Calendar.getInstance();
+			c.setTimeInMillis(timestamp);
+			return c;
+		}
+	}
+
+
+	private static SharedPreferences getSharedPreferences(Context context) {
+		return context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+	}
+
+
+	private static final String 
+		KEY_SUCCESS = "_SUCCESS",
+		KEY_FALIURE = "_FAILURE";
+
+	private static String toKey(OdsSource source, boolean success) {
+		if (success) return source.toString() + KEY_SUCCESS;
+		else return source.toString() + KEY_FALIURE;
+	}
+
+}

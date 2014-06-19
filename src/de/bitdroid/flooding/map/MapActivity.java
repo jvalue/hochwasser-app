38fac4a920663fd7e6844cd236c362d31b815a0f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-06-19 20:54:30 +0200

    map: start a center of stations

--- src/de/bitdroid/flooding/map/MapActivity.java
+++ src/de/bitdroid/flooding/map/MapActivity.java
@@ -7,8 +7,10 @@ import static de.bitdroid.flooding.pegelonline.PegelOnlineSource.COLUMN_STATION_
 import static de.bitdroid.flooding.pegelonline.PegelOnlineSource.COLUMN_WATER_NAME;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
@@ -18,6 +20,7 @@ import android.content.CursorLoader;
 import android.content.Loader;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.Point;
 import android.os.Bundle;
 
 import de.bitdroid.flooding.R;
@@ -33,7 +36,6 @@ public class MapActivity extends Activity implements MapConstants {
 	private MyLocationNewOverlay locationOverlay;
 	private FixedMapView mapView;
 	private StationsOverlay stationsOverlay;
-	private final List<Station> stations = new ArrayList<Station>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,23 +62,24 @@ public class MapActivity extends Activity implements MapConstants {
 				cursor.moveToFirst();
 				if (cursor.getCount() == 0) return;
 
+				List<Station> stations = new ArrayList<Station>();
+
 				int latIdx = cursor.getColumnIndex(COLUMN_STATION_LAT);
 				int longIdx = cursor.getColumnIndex(COLUMN_STATION_LONG);
 				int nameIdx = cursor.getColumnIndex(COLUMN_STATION_NAME);
 				int kmIdx = cursor.getColumnIndex(COLUMN_STATION_KM);
 
-				double xCenter = 0, yCenter = 0;
 				do {
 					String stationName = cursor.getString(nameIdx);
 					double km = cursor.getDouble(kmIdx);
 					double lat = cursor.getDouble(latIdx);
 					double lon = cursor.getDouble(longIdx);
 
-					xCenter += lat;
-					yCenter += lon;
-
 					stations.add(new Station(stationName, km, lat, lon));
 				} while (cursor.moveToNext());
+
+				// filter stations with invaild cooridnates
+				filterInvalidStations(stations);
 
 				// add to overlays
 				if (stationsOverlay != null) mapView.getOverlays().remove(stationsOverlay);
@@ -86,8 +89,9 @@ public class MapActivity extends Activity implements MapConstants {
 						stations);
 				mapView.getOverlays().add(stationsOverlay);
 
-				xCenter /= cursor.getCount();
-				yCenter /= cursor.getCount();
+				Point center = mapView.getProjection().toPixels(getCenter(stations), null);
+				mapView.scrollTo(center.x, center.y);
+				mapView.getController().setZoom(8);
 			}
 
 			@Override
@@ -157,11 +161,43 @@ public class MapActivity extends Activity implements MapConstants {
 	}
 
 	private void restoreMapState() {
+		/*
 		SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 		mapView.getController().setZoom(prefs.getInt(PREFS_ZOOM_LEVEL, 1));
 		mapView.scrollTo(
 				prefs.getInt(PREFS_SCROLL_X, 0),
 				prefs.getInt(PREFS_SCROLL_Y, 0));
+
+		Toast.makeText(this, "Zoom = " + mapView.getZoomLevel(), Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "Loc = " + mapView.getScrollX() + ", " + mapView.getScrollY(), Toast.LENGTH_LONG).show();
+		*/
+	}
+
+
+	private GeoPoint getCenter(List<Station> stations) {
+		double 
+			minX = Double.MAX_VALUE,
+			maxX = Double.MIN_VALUE,
+			minY = Double.MAX_VALUE,
+			maxY = Double.MIN_VALUE;
+
+		for (Station station : stations) {
+			minX = Math.min(station.getLat(), minX);
+			maxX = Math.max(station.getLat(), maxX);
+			minY = Math.min(station.getLon(), minY);
+			maxY = Math.max(station.getLon(), maxY);
+		}
+
+		return new GeoPoint((minX + maxX) / 2.0f, (minY + maxY) / 2.0f);
+	}
+
+
+	private void filterInvalidStations(List<Station> stations) {
+		Iterator<Station> iter = stations.iterator();
+		while (iter.hasNext()) {
+			Station s = iter.next();
+			if (s.getLat() == 0 && s.getLon() == 0) iter.remove();
+		}
 	}
 
 }

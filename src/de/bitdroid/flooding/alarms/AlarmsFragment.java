2307d814e4dbad6c43459d839e839551ff0f9828
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-18 20:21:30 +0200

    alarms: added menu entry for creating new alarms

--- src/de/bitdroid/flooding/alarms/AlarmsFragment.java
+++ src/de/bitdroid/flooding/alarms/AlarmsFragment.java
@@ -3,13 +3,23 @@ package de.bitdroid.flooding.alarms;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import de.bitdroid.flooding.R;
 
 
 public final class AlarmsFragment extends Fragment {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+	}
 
 
 	@Override
@@ -20,6 +30,23 @@ public final class AlarmsFragment extends Fragment {
 
 		View view = inflater.inflate(R.layout.alarms, container, false);
 		return view;
+	}
+
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.alarms_menu, menu);
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.add:
+				Toast.makeText(getActivity(), "stub", Toast.LENGTH_SHORT).show();
+				return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 }

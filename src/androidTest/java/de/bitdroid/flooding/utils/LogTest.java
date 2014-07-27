32ab04e2b01e5155124fa79f1e87977f71b426de
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-27 22:27:29 +0200

    test: more utils tests

---
+++ src/androidTest/java/de/bitdroid/flooding/utils/LogTest.java
@@ -0,0 +1,20 @@
+package de.bitdroid.flooding.utils;
+
+import junit.framework.TestCase;
+
+public class LogTest extends TestCase {
+
+    private static final String MSG = "test";
+    private static final Throwable ERROR = new RuntimeException(MSG);
+
+    public void testLog() {
+        Log.debug(MSG);
+        Log.debug(MSG, ERROR);
+        Log.info(MSG);
+        Log.info(MSG, ERROR);
+        Log.warning(MSG);
+        Log.warning(MSG, ERROR);
+        Log.error(MSG);
+        Log.error(MSG, ERROR);
+    }
+}

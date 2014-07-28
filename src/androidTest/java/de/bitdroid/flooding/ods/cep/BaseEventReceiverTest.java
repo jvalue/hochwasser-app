ffd920040131ba14232bfd2498aa713f4ffb24d0
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-28 19:33:44 +0200

    test: added BaseEventReceiverTest

---
+++ src/androidTest/java/de/bitdroid/flooding/ods/cep/BaseEventReceiverTest.java
@@ -0,0 +1,37 @@
+package de.bitdroid.flooding.ods.cep;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.test.AndroidTestCase;
+
+public class BaseEventReceiverTest extends AndroidTestCase {
+
+	private static final String
+			EPL_STMT = "select * from *",
+			EVENT_ID = "eventId";
+
+	private int callCounter = 0;
+
+	public void testParameters() {
+		BroadcastReceiver receiver = new BaseEventReceiver() {
+
+			@Override
+			public void onReceive(Context context, String eplStmt, String eventId) {
+				assertNotNull(context);
+				assertEquals(EPL_STMT, eplStmt);
+				assertEquals(EVENT_ID, eventId);
+				callCounter++;
+			}
+
+		};
+
+		Intent intent = new Intent();
+		intent.putExtra(BaseEventReceiver.EXTRA_EPLSTMT, EPL_STMT);
+		intent.putExtra(BaseEventReceiver.EXTRA_EVENTID, EVENT_ID);
+
+		receiver.onReceive(getContext(), intent);
+		assertEquals(1, callCounter);
+	}
+
+}

885f3299166fbbacf081acd1e21a097bb1c28769
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-30 16:51:36 +0200

    test: added CopySourceServiceTest

---
+++ app/src/androidTest/java/de/bitdroid/flooding/monitor/CopySourceServiceTest.java
@@ -0,0 +1,163 @@
+package de.bitdroid.flooding.monitor;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.test.RenamingDelegatingContext;
+import android.test.ServiceTestCase;
+import android.test.mock.MockContentResolver;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import de.bitdroid.flooding.R;
+import de.bitdroid.flooding.ods.data.OdsSource;
+import de.bitdroid.flooding.utils.SQLiteType;
+
+public class CopySourceServiceTest extends ServiceTestCase {
+
+
+	public CopySourceServiceTest() {
+		super(CopySourceService.class);
+	}
+
+
+	public void testCopyAndDelete() throws Exception {
+		// setup content provider and data
+		MockContentResolver resolver = new MockContentResolver();
+		setContext(new MockContentProviderContext(getContext(), resolver));
+
+		ListContentProvider provider = new ListContentProvider();
+		provider.addCursor(MockOdsSource.getSampelCursor());
+		provider.addCursor(MockOdsSource.getSampelCursor());
+		provider.addCursor(MockOdsSource.getSampelCursor());
+		provider.addCursor(MockOdsSource.getSampelCursor());
+		resolver.addProvider(OdsSource.AUTHORITY, provider);
+
+		OdsSource source = new MockOdsSource();
+		SourceMonitor monitor = SourceMonitor.getInstance(getContext());
+
+		// setup monitor frequency
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+		editor.putInt(getContext().getString(R.string.prefs_ods_monitor_days_key), 1);
+		editor.putFloat(getContext().getString(R.string.prefs_ods_monitor_interval_key), 11.5f);
+		editor.commit();
+
+		// start first copying
+		monitor.startMonitoring(source);
+
+		Intent intent = new Intent(getContext(), CopySourceService.class);
+		intent.putExtra(CopySourceService.EXTRA_SOURCE_NAME, source.toString());
+
+		assertEquals(0, monitor.getAvailableTimestamps(source).size());
+
+		startService(intent);
+		Thread.sleep(100);
+		assertEquals(1, monitor.getAvailableTimestamps(source).size());
+
+		startService(intent);
+		Thread.sleep(100);
+		assertEquals(2, monitor.getAvailableTimestamps(source).size());
+
+		// old values should have been deleted
+		startService(intent);
+		Thread.sleep(100);
+		assertEquals(2, monitor.getAvailableTimestamps(source).size());
+
+		startService(intent);
+		Thread.sleep(100);
+		assertEquals(2, monitor.getAvailableTimestamps(source).size());
+	}
+
+
+	private static class MockContentProviderContext extends RenamingDelegatingContext {
+
+		private final ContentResolver resolver;
+
+		public MockContentProviderContext(Context targetContext, ContentResolver resolver) {
+			super(targetContext, "test");
+			this.resolver = resolver;
+		}
+
+		@Override
+		public ContentResolver getContentResolver() {
+			return resolver;
+		}
+
+		@Override
+		public Context getApplicationContext() {
+			return this;
+		}
+
+	}
+
+	private static class ListContentProvider extends android.test.mock.MockContentProvider {
+
+		private final List<Cursor> cursors = new LinkedList<Cursor>();
+
+		public void addCursor(Cursor cursor) {
+			cursors.add(cursor);
+		}
+
+		@Override
+		public Cursor query(
+				Uri uri,
+				String[] projection,
+				String selection,
+				String[] selectionArgs,
+				String sortOrder) {
+
+			if (cursors.size() == 0) return null;
+			return cursors.remove(0);
+		}
+	}
+
+	public static class MockOdsSource extends OdsSource {
+
+		private static final String COLUMN = "value";
+
+		@Override
+		public String getSourceUrlPath() {
+			return "someUrl";
+		}
+
+		@Override
+		public String getSourceId() {
+			return "someId";
+		}
+
+		@Override
+		public Map<String, SQLiteType> getSchema() {
+			Map<String, SQLiteType> schema = new HashMap<String, SQLiteType>();
+			schema.put(COLUMN, SQLiteType.TEXT);
+			return schema;
+		}
+
+		@Override
+		public ContentValues saveData(JSONObject json) {
+			throw new UnsupportedOperationException("stub");
+		}
+
+		public static Cursor getSampelCursor() {
+			MatrixCursor cursor = new MatrixCursor(new String[] {
+					OdsSource.COLUMN_SERVER_ID,
+					COLUMN });
+			for (int i = 0; i < 10; i++) {
+				String randomValue = String.valueOf(Math.random());
+				cursor.addRow(new String[] { randomValue, randomValue });
+			}
+			return cursor;
+		}
+	}
+
+}

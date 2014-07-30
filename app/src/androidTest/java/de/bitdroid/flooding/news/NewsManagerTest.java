f7c40ee7f27002dab057908be49d5539a277185f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-07-30 12:51:16 +0200

    test: fixed NewsManagerTest

--- app/src/androidTest/java/de/bitdroid/flooding/news/NewsManagerTest.java
+++ app/src/androidTest/java/de/bitdroid/flooding/news/NewsManagerTest.java
@@ -13,8 +13,6 @@ public final class NewsManagerTest extends AndroidTestCase {
 		NewsManager manager = NewsManager.getInstance(context);
 
 		assertTrue(manager.getAllItems().isEmpty());
-		assertTrue(manager.getUnreadItems().isEmpty());
-		assertTrue(manager.getReadItems().isEmpty());
 
 		manager.markAllItemsRead();
 		assertTrue(manager.getAllItems().isEmpty());
@@ -23,29 +21,27 @@ public final class NewsManagerTest extends AndroidTestCase {
 		NewsItem i2 = new NewsItem.Builder("hello", "world", 21).setNavigationPos(42).build();
 
 		manager.addItem(i1, false);
-		assertTrue(manager.getAllItems().contains(i1));
+		assertTrue(manager.getAllItems().containsKey(i1));
+		assertFalse(manager.getAllItems().get(i1));
 		assertTrue(manager.getAllItems().size() == 1);
-		assertTrue(manager.getUnreadItems().size() == 1);
-		assertTrue(manager.getReadItems().size() == 0);
 
 		manager.addItem(i2, false);
-		assertTrue(manager.getAllItems().contains(i1));
-		assertTrue(manager.getAllItems().contains(i2));
+		assertTrue(manager.getAllItems().containsKey(i1));
+		assertTrue(manager.getAllItems().containsKey(i2));
+		assertFalse(manager.getAllItems().get(i1));
+		assertFalse(manager.getAllItems().get(i2));
 		assertTrue(manager.getAllItems().size() == 2);
-		assertTrue(manager.getUnreadItems().size() == 2);
-		assertTrue(manager.getReadItems().size() == 0);
 
 		manager.markAllItemsRead();
-		assertTrue(manager.getUnreadItems().size() == 0);
-		assertTrue(manager.getReadItems().size() == 2);
+		assertTrue(manager.getAllItems().get(i1));
+		assertTrue(manager.getAllItems().get(i2));
 
 		manager.removeItem(i1);
-		assertTrue(!manager.getAllItems().contains(i1));
-		assertTrue(!manager.getReadItems().contains(i1));
+		assertFalse(manager.getAllItems().containsKey(i1));
+		assertTrue(manager.getAllItems().containsKey(i2));
 		assertTrue(manager.getAllItems().size() == 1);
 
 		manager.removeItem(i2);
-		assertTrue(!manager.getAllItems().contains(i2));
 		assertTrue(manager.getAllItems().size() == 0);
 	}
 

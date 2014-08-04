36d792c0c3e9991ede66e68cf64380b441d4ff8f
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-08-04 11:02:38 +0200

    test: minor cleanup in PrefsRenamingDelegatingContext

--- app/src/androidTest/java/de/bitdroid/flooding/testUtils/PrefsRenamingDelegatingContext.java
+++ app/src/androidTest/java/de/bitdroid/flooding/testUtils/PrefsRenamingDelegatingContext.java
@@ -1,13 +1,9 @@
 package de.bitdroid.flooding.testUtils;
 
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.test.RenamingDelegatingContext;
-import android.test.mock.MockContext;
 
 public class PrefsRenamingDelegatingContext extends RenamingDelegatingContext {
 
@@ -15,15 +11,7 @@ public class PrefsRenamingDelegatingContext extends RenamingDelegatingContext {
 	private final String prefix;
 
 	public PrefsRenamingDelegatingContext(Context targetContext, String prefix) {
-		// this(new MockDelegatingContext(targetContext), prefix);
 		super(targetContext, prefix);
-		this.targetContext = targetContext;
-		this.prefix = prefix;
-	}
-
-
-	public PrefsRenamingDelegatingContext(MockDelegatingContext targetContext, String prefix) {
-		super(new MockDelegatingContext(targetContext), prefix);
 		this.targetContext = targetContext;
 		this.prefix = prefix;
 	}
@@ -42,7 +30,6 @@ public class PrefsRenamingDelegatingContext extends RenamingDelegatingContext {
 	}
 
 
-
 	String getPrefix() {
 		return prefix;
 	}
@@ -52,35 +39,4 @@ public class PrefsRenamingDelegatingContext extends RenamingDelegatingContext {
 		return targetContext;
 	}
 
-
-	private static final class MockDelegatingContext extends MockContext {
-
-		private final Context context;
-
-		public MockDelegatingContext(Context context) {
-			this.context = context;
-		}
-
-
-		@Override
-		public SharedPreferences getSharedPreferences(String prefsName, int mode) {
-			return context.getSharedPreferences(prefsName, mode);
-		}
-
-		@Override
-		public Intent registerReceiver(BroadcastReceiver receiver, IntentFilter filter) {
-			return context.registerReceiver(receiver, filter);
-		}
-
-		@Override
-		public void unregisterReceiver(BroadcastReceiver receiver) {
-			context.unregisterReceiver(receiver);
-		}
-
-		@Override
-		public String getPackageName() {
-			return context.getPackageName();
-		}
-
-	}
 }

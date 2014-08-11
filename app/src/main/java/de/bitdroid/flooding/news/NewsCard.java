b1ff9d5032431b2623b9cf447a2691a5efede083
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2014-08-11 21:48:20 +0200

    news: added overflow button for sharing and deleting cards

--- app/src/main/java/de/bitdroid/flooding/news/NewsCard.java
+++ app/src/main/java/de/bitdroid/flooding/news/NewsCard.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.text.Html;
 import android.text.format.DateFormat;
 import android.util.Pair;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -14,6 +15,8 @@ import java.util.Date;
 import de.bitdroid.flooding.R;
 import de.bitdroid.flooding.main.MainActivity;
 import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.base.BaseCard;
 
 final class NewsCard extends Card {
 
@@ -51,18 +54,46 @@ final class NewsCard extends Card {
 				}
 			});
 		}
+
+		// custom header with overflow button
+		CardHeader header = new CardHeader(getContext(), R.layout.news_card_header) {
+			@Override
+			public void setupInnerViewElements(ViewGroup parent, View view) {
+				TextView title = (TextView) parent.findViewById(R.id.news_title);
+				title.setText(data.first.getTitle());
+			}
+		};
+
+		header.setPopupMenu(R.menu.news_card_menu, new CardHeader.OnClickCardHeaderPopupMenuListener() {
+			@Override
+			public void onMenuItemClick(BaseCard card, MenuItem item) {
+				switch (item.getItemId()) {
+					case R.id.share:
+						Intent intent = new Intent();
+						intent.setAction(Intent.ACTION_SEND);
+						intent.putExtra(Intent.EXTRA_TEXT, data.first.getContent());
+						intent.setType("text/plain");
+						activity.startActivity(Intent.createChooser(intent, getContext().getResources().getText(R.string.share_with)));
+						return;
+
+					case R.id.delete:
+						manager.removeItem(data.first);
+						return;
+				}
+			}
+		});
+
+		addCardHeader(header);
 	}
 
 
 	@Override
 	public void setupInnerViewElements(ViewGroup parent, View view) {
-		TextView title = (TextView) parent.findViewById(R.id.news_title);
 		TextView msg  = (TextView) parent.findViewById(R.id.news_timestamp);
 		TextView content = (TextView) parent.findViewById(R.id.news_content);
 
 		Date date = new Date(data.first.getTimestamp());
 
-		title.setText(data.first.getTitle());
 		msg.setText(DateFormat.getDateFormat(getContext()).format(date)
 			+ " " + DateFormat.getTimeFormat(getContext()).format(date));
 		content.setText(Html.fromHtml(data.first.getContent()));

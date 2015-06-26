cc7961d76d49f5eb1439215d9078bc1f2070d7ba
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-06-26 14:00:47 +0200

    ui: add swipe to refresh to StationInfoActivity

--- app/src/main/java/de/bitdroid/flooding/ui/StationInfoActivity.java
+++ app/src/main/java/de/bitdroid/flooding/ui/StationInfoActivity.java
@@ -2,6 +2,7 @@ package de.bitdroid.flooding.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -26,7 +27,7 @@ import rx.functions.Action1;
  * Displays information about a single station.
  */
 @ContentView(R.layout.activity_station_info)
-public class StationInfoActivity extends AbstractRestrictedActivity {
+public class StationInfoActivity extends AbstractRestrictedActivity implements SwipeRefreshLayout.OnRefreshListener {
 
 	private static final String STATE_MEASUREMENTS = "STATE_MEASUREMENTS";
 
@@ -34,6 +35,7 @@ public class StationInfoActivity extends AbstractRestrictedActivity {
 	@InjectView(R.id.card_char_values) private CardView charValuesCard;
 	@InjectView(R.id.card_metadata) private CardView metadataCard;
 	@InjectView(R.id.card_map) private CardView mapCard;
+	@InjectView(R.id.refresh_layout) private SwipeRefreshLayout refreshLayout;
 
 	@Inject private OdsManager odsManager;
 	@Inject private StationInfoUtils stationInfoUtils;
@@ -52,28 +54,37 @@ public class StationInfoActivity extends AbstractRestrictedActivity {
 
 		// load data
 		if (savedInstanceState != null && savedInstanceState.get(STATE_MEASUREMENTS) != null) {
-			setupData((StationMeasurements) savedInstanceState.getParcelable(STATE_MEASUREMENTS));
+			displayData((StationMeasurements) savedInstanceState.getParcelable(STATE_MEASUREMENTS));
 
 		} else {
 			showSpinner();
-			compositeSubscription.add(odsManager.getMeasurements(station)
-					.compose(networkUtils.<StationMeasurements>getDefaultTransformer())
-					.subscribe(new Action1<StationMeasurements>() {
-						@Override
-						public void call(StationMeasurements stationMeasurements) {
-							hideSpinner();
-							setupData(stationMeasurements);
-						}
-					}, new ErrorActionBuilder()
-							.add(new DefaultErrorAction(this, this, "failed to download measurements"))
-							.add(new HideSpinnerAction(this))
-							.build()));
+			loadData();
 		}
 
+		// setup swipe to refresh
+		refreshLayout.setOnRefreshListener(this);
+		refreshLayout.setColorSchemeResources(R.color.blue_light, R.color.blue, R.color.blue_dark);
 	}
 
 
-	private void setupData(StationMeasurements measurements) {
+	private void loadData() {
+		compositeSubscription.add(odsManager.getMeasurements(station)
+				.compose(networkUtils.<StationMeasurements>getDefaultTransformer())
+				.subscribe(new Action1<StationMeasurements>() {
+					@Override
+					public void call(StationMeasurements stationMeasurements) {
+						if (isSpinnerVisible()) hideSpinner();
+						if (refreshLayout.isRefreshing()) refreshLayout.setRefreshing(false);
+						displayData(stationMeasurements);
+					}
+				}, new ErrorActionBuilder()
+						.add(new DefaultErrorAction(this, this, "failed to download measurements"))
+						.add(new HideSpinnerAction(this))
+						.build()));
+	}
+
+
+	private void displayData(StationMeasurements measurements) {
 		this.measurements = measurements;
 		stationInfoUtils.setupStationCards(measurements, levelsCard, charValuesCard, metadataCard, mapCard);
 	}
@@ -105,6 +116,15 @@ public class StationInfoActivity extends AbstractRestrictedActivity {
 		}
 
 		return super.onOptionsItemSelected(item);
+	}
+
+
+	@Override
+	public void onRefresh() {
+		// ignore if still loading
+		if (isSpinnerVisible()) return;
+		refreshLayout.setRefreshing(true);
+		loadData();
 	}
 
 }

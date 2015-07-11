5b573a1a83fc1d9b3b9f8d630a69cb05466b9694
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-07-11 12:11:03 +0200

    Configure crashlytics

--- app/src/main/java/de/bitdroid/flooding/app/FloodingApplication.java
+++ app/src/main/java/de/bitdroid/flooding/app/FloodingApplication.java
@@ -1,11 +1,13 @@
 package de.bitdroid.flooding.app;
 
 
+import com.crashlytics.android.Crashlytics;
 import com.orm.SugarApp;
 
 import de.bitdroid.flooding.BuildConfig;
 import de.bitdroid.flooding.ceps.CepsModule;
 import de.bitdroid.flooding.ods.OdsModule;
+import io.fabric.sdk.android.Fabric;
 import roboguice.RoboGuice;
 import timber.log.Timber;
 
@@ -28,9 +30,44 @@ public class FloodingApplication extends SugarApp {
 		if (BuildConfig.DEBUG) {
 			Timber.plant(new Timber.DebugTree());
 		} else {
-			throw new RuntimeException("configure crashlytics and logging");
+			Fabric.with(this, new Crashlytics());
+			Timber.plant(new CrashReportingTree());
 		}
 
 	}
+
+
+	private static final class CrashReportingTree extends Timber.Tree {
+
+		@Override
+		public void e(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void e(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
+		@Override
+		public void w(String msg, Object... args) {
+			Crashlytics.log(msg);
+		}
+
+		@Override
+		public void w(Throwable e, String msg, Object... args) {
+			Crashlytics.log(msg);
+			Crashlytics.logException(e);
+		}
+
+
+		@Override
+		protected void log(int priority, String tag, String message, Throwable t) {
+			// nothing to do here
+		}
+
+	}
+
 
 }

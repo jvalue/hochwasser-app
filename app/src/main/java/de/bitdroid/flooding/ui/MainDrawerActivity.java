b6f0a866b94940c0d8b0ef5358d57df083a7b1c3
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-08-06 18:40:30 +0200

    ui: remove unused code frokm MainDrawerActivity

--- app/src/main/java/de/bitdroid/flooding/ui/MainDrawerActivity.java
+++ app/src/main/java/de/bitdroid/flooding/ui/MainDrawerActivity.java
@@ -57,72 +57,10 @@ public class MainDrawerActivity extends AbstractActivity implements Drawer.OnDra
 		super.onCreate(savedInstanceState);
 		setSupportActionBar(toolbar);
 
-		/*
-		// setup drawer header
-		View drawerHeader = LayoutInflater.from(this).inflate(R.layout.drawer_header, null);
-		TextView emailView = (TextView) drawerHeader.findViewById(R.id.text_email);
-		Optional<Account> account = loginManager.getAccount();
-		if (account.isPresent()) emailView.setText(account.get().name);
-		setDrawerHeaderCustom(drawerHeader);
-
-		// setup main sections
-		addSection(newSection(getString(R.string.nav_home), R.drawable.ic_home, new NewsFragment()));
-		Intent intent = new Intent(this, DataSelectionHandler.WaterSelectionActivity.class);
-		addSection(newSection(getString(R.string.nav_alarms), R.drawable.ic_alert, new AlarmsFragment()));
-		addSection(newSection(getString(R.string.nav_analysis), R.drawable.ic_chart, intent));
-
-		// setup settings and feedback section
-		addBottomSection(newSection(getString(R.string.nav_settings), R.drawable.ic_settings, new SettingsFragment()));
-
-		String address = getString(R.string.feedback_mail_address);
-		String subject = getString(
-				R.string.feedback_mail_subject,
-				getString(R.string.app_name),
-				versionUtils.getVersion());
-		Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", address, null));
-		mailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
-		Intent mailer = Intent.createChooser(mailIntent, getString(R.string.feedback_mail_chooser));
-		addBottomSection(newSection(getString(R.string.nav_feedback), R.drawable.ic_email, mailer));
-		*/
-
-
-
-
 		// show first fragment
 		showFragment(new NewsFragment(), false);
 		toolbar.setTitle(R.string.nav_home);
 		visibleFragmentId = ID_NEWS;
-
-		/*
-		// setup image loading for nav drawer
-		DrawerImagekoader.init(new DrawerImageLoader.IDrawerImageLoader() {
-			@Override
-			public void set(ImageView imageView, Uri uri, Drawable drawable) {
-				Picasso.with(imageView.getContext()).load(uri).into(imageView);
-			}
-
-			@Override
-			public void cancel(ImageView imageView) {
-				Picasso.with(imageView.getContext()).cancelRequest(imageView);
-			}
-
-			@Override
-			public Drawable placeholder(Context context) {
-				return null;
-			}
-		});
-
-		Account account = authManager.getAccount();
-		AccountHeader accountHeader = new AccountHeaderBuilder()
-				.withActivity(this)
-				.withHeaderBackground(R.drawable.background)
-				.addProfiles(
-						new ProfileDrawerItem().withName(account.getName()).withEmail(account.getEmail()).withIcon(account.getImageUrl())
-				)
-				.withProfileImagesClickable(false)
-				.withSelectionListEnabledForSingleProfile(false)
-				.build();
-				*/
 
 		// setup account in nav drawer
 		Account account = loginManager.getAccount().get();
@@ -151,15 +89,6 @@ public class MainDrawerActivity extends AbstractActivity implements Drawer.OnDra
 				)
 				.withOnDrawerItemClickListener(this)
 				.build();
-
-		/*
-		// setup account in nav drawer
-		TextView emailView = (TextView) drawer.getHeader().findViewById(R.id.text_email);
-		// View drawerHeader = LayoutInflater.from(this).inflate(R.layout.drawer_header, null);
-		// TextView emailView = (TextView) drawerHeader.findViewById(R.id.text_email);
-		Optional<Account> account = loginManager.getAccount();
-		if (account.isPresent()) emailView.setText(account.get().name);
-		*/
 	}
 
 

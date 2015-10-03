68573c6719b4b6a09dc0fd31a272bfc390a799a7
Philipp Eichhorn <philipp.eichhorn@bitdroid.de>
2015-10-03 19:39:28 +0200

    ui: filter rivers with low station count

--- app/src/main/java/de/bitdroid/flooding/ui/WaterGraphSelectionHandler.java
+++ app/src/main/java/de/bitdroid/flooding/ui/WaterGraphSelectionHandler.java
@@ -3,8 +3,13 @@ package de.bitdroid.flooding.ui;
 
 import android.content.Intent;
 
+import java.util.Iterator;
+import java.util.List;
+
 import de.bitdroid.flooding.ods.BodyOfWater;
 import de.bitdroid.flooding.ui.graph.WaterGraphActivity;
+import rx.Observable;
+import rx.functions.Func1;
 
 
 /**
@@ -24,6 +29,21 @@ public class WaterGraphSelectionHandler {
 		protected void onWaterSelected(BodyOfWater water) {
 			Intent graphIntent = new StationSelection(water).toIntent(this, WaterGraphActivity.class);
 			startActivity(graphIntent);
+		}
+
+		@Override
+		protected Observable<List<BodyOfWater>> doLoadItems() {
+			return super.doLoadItems()
+					.map(new Func1<List<BodyOfWater>, List<BodyOfWater>>() {
+						@Override
+						public List<BodyOfWater> call(List<BodyOfWater> bodyOfWaters) {
+							Iterator<BodyOfWater> iterator = bodyOfWaters.iterator();
+							while (iterator.hasNext()) {
+								if (iterator.next().getStationCount() <= 1) iterator.remove();
+							}
+							return bodyOfWaters;
+						}
+					});
 		}
 
 		@Override
